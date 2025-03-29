4873c9f205fb2800f7997ef04215e4a655c27747
Sandeep Dutta <quark01@gmail.com>
2025-03-28 17:26:36 -0700

    changed to service

--- main/main.cpp
+++ main/main.cpp
@@ -14,6 +14,7 @@
 #include <rcl/error_handling.h>
 #include <std_msgs/msg/int32.h>
 #include <sensor_msgs/msg/channel_float32.h>
+#include <jeeves_msgs/srv/led_set.h>
 #include <rclc/rclc.h>
 #include <rclc/executor.h>
 
@@ -29,9 +30,7 @@
 #define RCSOFTCHECK(fn) { rcl_ret_t temp_rc = fn; if((temp_rc != RCL_RET_OK)){printf("Failed status on line %d: %d. Continuing.\n",__LINE__,(int)temp_rc);}}
 #define MAX_LEDS 16
 rcl_publisher_t publisher;
-rcl_subscription_t subscriber;
 std_msgs__msg__Int32 send_msg;
-sensor_msgs__msg__ChannelFloat32 recv_msg;
 static led_strip_handle_t led_strip;
 static bool save_mode = false;
 
@@ -43,15 +42,6 @@ typedef struct {
 
 static led_values_t led_values[MAX_LEDS];
 static led_values_t led_values_backup[MAX_LEDS];
-static led_values_t flash_color;
-static int flash_count = 0;
-static bool flash_on = false;
-
-enum led_state {
-	LED_STATE_NORMAL,
-	LED_STATE_FLASHING
-};
-static int led_state = LED_STATE_NORMAL;
 
 static void set_leds_to_color(int red, int green, int blue) {
 	for (size_t i = 0; i < MAX_LEDS; i++) {
@@ -74,132 +64,89 @@ void hb_timer_callback(rcl_timer_t * timer, int64_t last_call_time)
 	}
 }
 
-// led timer callback
-void led_timer_callback(rcl_timer_t * timer, int64_t last_call_time)
-{
-	(void) last_call_time;
-	if (led_state == LED_STATE_FLASHING) {
-		flash_count--;
-		// flashing done restore original colors
-		if (flash_count == 0) {
-			led_state = LED_STATE_NORMAL;
-			flash_on = false;
-			// restore original colors
-			for (size_t i = 0; i < MAX_LEDS; i++) {
-				led_strip_set_pixel(led_strip, i, led_values[i].red, led_values[i].green, led_values[i].blue);
-			}
-		} else if (flash_on) { // Flash on turn off
-			flash_on = false;
-			// flash off
-			led_strip_clear(led_strip);
-			return;
-		} else { // Flash off turn on
-			// flash on
-			flash_on = true;
-			for (size_t i = 0; i < MAX_LEDS; i++) {
-				led_strip_set_pixel(led_strip, i, flash_color.red, flash_color.green, flash_color.blue);
-			}
+static void flash_leds(int times, int delay_ticks, int min_led, int max_led, 	int red, int green, int blue) {
+	// Backup current LED state
+	for (int led = min_led; led <= max_led; led++) {
+		led_values_backup[led] = led_values[led];
+	}
+	for (int i = 0; i < times; i++) {
+		// Turn on LEDs
+		for (int led = min_led; led <= max_led; led++) {
+			led_strip_set_pixel(led_strip, led, red, green, blue);
+		}	
+		led_strip_refresh(led_strip);
+		vTaskDelay(delay_ticks);
+		// Turn off LEDs
+		for (int led = min_led; led <= max_led; led++) {
+			led_strip_set_pixel(led_strip, led, 0, 0, 0);
 		}
 		led_strip_refresh(led_strip);
+		vTaskDelay(delay_ticks);
 	}
+	// Restore original LED state
+	for (int led = min_led; led <= max_led; led++) {
+		led_values[led] = led_values_backup[led];
+		led_strip_set_pixel(led_strip, led, led_values[led].red, led_values[led].green, led_values[led].blue);
+	}
+	led_strip_refresh(led_strip);
 }
 
-void subscription_callback(const void * msgin)
-{
-	const sensor_msgs__msg__ChannelFloat32 * msg = (const sensor_msgs__msg__ChannelFloat32 *)msgin;
-	printf("Received: %s %d\n", msg->name.data, msg->values.size);
-	// Set individual LED states	
-	if (strcmp(msg->name.data, "led") == 0) {
-		printf("LED state: %f\n", msg->values.data[0]);
-		//value size must be divisible by 4
-		if (msg->values.size % 4 != 0) {
-			printf("Invalid LED state message\n");
-			return;
-		}
-		// if first led_number is -1, set all LEDs to color
-		if ((int)msg->values.data[0] == -1) {
-			set_leds_to_color(msg->values.data[1], msg->values.data[2], msg->values.data[3]);
-			led_state = LED_STATE_NORMAL;
+// service callback
+void service_callback(const void * req, void * res){
+	jeeves_msgs__srv__LedSet_Response * res_out = (jeeves_msgs__srv__LedSet_Response *)res;
+	jeeves_msgs__srv__LedSet_Request * req_in = (jeeves_msgs__srv__LedSet_Request *)req;
+	printf("Received LED set request %ld %ld %ld %ld %ld\n", req_in->led_cmd, req_in->led_number, req_in->red, req_in->green, req_in->blue);
+	int flash_delay = req_in->flash_speed / portTICK_PERIOD_MS;
+	res_out->success = true;
+	switch (req_in->led_cmd) {
+		case jeeves_msgs__srv__LedSet_Request__LED_SET_ALL:
+			set_leds_to_color(req_in->red, req_in->green, req_in->blue);
 			led_strip_refresh(led_strip);
-			return;
-		}
-		// each LED has 4 elements led_number, red, green, blue
-		// led_number is the index of the LED
-		// red, green, blue are the color values
-		auto num_leds = msg->values.size / 4;
-		for (size_t i = 0; i < num_leds; i++) {
-			int led_number = msg->values.data[i*4];
-			int red = msg->values.data[i*4+1];
-			int green = msg->values.data[i*4+2];
-			int blue = msg->values.data[i*4+3];
-			// save values to led_values
-			led_values[led_number].red = red;	
-			led_values[led_number].green = green;
-			led_values[led_number].blue = blue;
-			// set pixel
-			led_strip_set_pixel(led_strip, led_number, red, green, blue);
-		}
-		led_state = LED_STATE_NORMAL;
-		led_strip_refresh(led_strip);
-	} else if (strcmp(msg->name.data, "led_fb") == 0 && !save_mode) { // Set front and back LEDs to color : back LED is +8 from front 
-		//value size must be divisible by 4
-		if (msg->values.size % 4 != 0) {
-			printf("Invalid LED state message\n");
-			return;
-		}
-		// each LED has 4 elements led_number, red, green, blue
-		// led_number is the index of the LED
-		// red, green, blue are the color values
-		int num_leds = msg->values.size / 4;
-		for (size_t i = 0; i < num_leds; i++) {
-			int led_number = msg->values.data[i*4];
-			int red = msg->values.data[i*4+1];
-			int green = msg->values.data[i*4+2];
-			int blue = msg->values.data[i*4+3];
-			// save values to led_values
-			led_values[led_number].red = led_values[led_number+8].red = red;	
-			led_values[led_number].green = led_values[led_number+8].green = green;
-			led_values[led_number].blue = led_values[led_number+8].blue = blue;
-			// set pixel
-			led_strip_set_pixel(led_strip, led_number, red, green, blue); // front
-			led_strip_set_pixel(led_strip, led_number+8, red, green, blue); // back
-		}
-		led_state = LED_STATE_NORMAL;
-		led_strip_refresh(led_strip);
-	} else if (strcmp(msg->name.data, "led_clear") == 0 && !save_mode) {
-		set_leds_to_color(0, 0, 0);
-		led_strip_clear(led_strip);
-		led_state = LED_STATE_NORMAL;
-	} else if (strcmp(msg->name.data, "led_backup") == 0) {
-		save_mode = true;
-		for (size_t i = 0; i < MAX_LEDS; i++) {
-			led_values_backup[i] = led_values[i];
-		}
-	} else if (strcmp(msg->name.data, "led_restore") == 0) {	
-		save_mode = false;
-		for (size_t i = 0; i < MAX_LEDS; i++) {
-			led_values[i] = led_values_backup[i];
-			led_strip_set_pixel(led_strip, i, led_values[i].red, led_values[i].green, led_values[i].blue);
-		}
-		led_state = LED_STATE_NORMAL;
-		led_strip_refresh(led_strip);
-	} else if (strcmp(msg->name.data, "led_flash") == 0 && !save_mode) {
-		//value size must be divisible by 4
-		if (msg->values.size % 4 != 0) {
-			printf("Invalid LED flash message\n");
-			return;
-		}
-		led_state = LED_STATE_FLASHING;
-		// first value number of flashes
-		flash_count = (int) msg->values.data[0];
-		// second value is the color
-		flash_color.red = (int) msg->values.data[1];
-		flash_color.green = (int) msg->values.data[2];
-		flash_color.blue = (int) msg->values.data[3];
-		// rest handled by led timer callback
+			break;
+		case jeeves_msgs__srv__LedSet_Request__LED_SINGLE	:
+			led_values[req_in->led_number].red = req_in->red;
+			led_values[req_in->led_number].green = req_in->green;
+			led_values[req_in->led_number].blue = req_in->blue;
+			led_strip_set_pixel(led_strip, req_in->led_number, req_in->red, req_in->green, req_in->blue);
+			led_strip_refresh(led_strip);
+			break;
+		case jeeves_msgs__srv__LedSet_Request__LED_FB:
+			led_values[req_in->led_number].red = led_values[req_in->led_number+8].red = req_in->red;
+			led_values[req_in->led_number].green = led_values[req_in->led_number+8].green = req_in->green;
+			led_values[req_in->led_number].blue = led_values[req_in->led_number+8].blue = req_in->blue;
+			led_strip_set_pixel(led_strip, req_in->led_number, req_in->red, req_in->green, req_in->blue);
+			led_strip_set_pixel(led_strip, req_in->led_number+8, req_in->red, req_in->green, req_in->blue);
+			led_strip_refresh(led_strip);
+			break;
+		case jeeves_msgs__srv__LedSet_Request__LED_FLASH:
+			flash_leds(req_in->flash_times, flash_delay, 0, MAX_LEDS-1, req_in->red, req_in->green, req_in->blue);
+			break;
+		case jeeves_msgs__srv__LedSet_Request__LED_FLASH_F:
+			flash_leds(req_in->flash_times, flash_delay, 0, (MAX_LEDS/2)-1, req_in->red, req_in->green, req_in->blue);
+			break;
+		case jeeves_msgs__srv__LedSet_Request__LED_FLASH_B:
+			flash_leds(req_in->flash_times, flash_delay, MAX_LEDS/2, MAX_LEDS-1, req_in->red, req_in->green, req_in->blue);
+			break;
+		case jeeves_msgs__srv__LedSet_Request__LED_SAVE:
+			for (int led = 0; led < MAX_LEDS; led++) {
+				led_values_backup[led] = led_values[led];
+			}
+			save_mode = true;
+			break;
+		case jeeves_msgs__srv__LedSet_Request__LED_RESTORE:	
+			for (int led = 0; led < MAX_LEDS; led++) {
+				led_values[led] = led_values_backup[led];
+				led_strip_set_pixel(led_strip, led, led_values[led].red, led_values[led].green, led_values[led].blue);
+			}
+			led_strip_refresh(led_strip);
+			save_mode = false;
+			break;	
+		default:
+			printf("Invalid LED command\n");
+			res_out->success = false;
+			break;
 	}
 }
-
 
 void micro_ros_task(void * arg)
 {
@@ -238,7 +185,7 @@ void micro_ros_task(void * arg)
 	conf.max_string_capacity = 50;
 	conf.max_ros2_type_sequence_capacity = MAX_LEDS*4;
 	conf.max_basic_type_sequence_capacity = MAX_LEDS*4;
-
+	/*
 	bool success = micro_ros_utilities_create_message_memory(
 		ROSIDL_GET_MSG_TYPE_SUPPORT(sensor_msgs, msg, ChannelFloat32),
 		&recv_msg,
@@ -248,6 +195,7 @@ void micro_ros_task(void * arg)
 		printf("Failed to allocate memory for incoming messages\n");
 		vTaskDelete(NULL);
 	}
+	*/
 	// Create node.
 	rcl_node_t node = rcl_get_zero_initialized_node();
 	RCCHECK(rclc_node_init_default(&node, "jeeves_esp32", "", &support));
@@ -259,13 +207,12 @@ void micro_ros_task(void * arg)
 		ROSIDL_GET_MSG_TYPE_SUPPORT(std_msgs, msg, Int32),
 		"~/heartbeat"));
 
-	// Create subscriber.
-	RCCHECK(rclc_subscription_init_default(
-		&subscriber,
-		&node,
-		ROSIDL_GET_MSG_TYPE_SUPPORT(sensor_msgs, msg, ChannelFloat32),
-		"~/led_state"));
-
+    // create service
+    rcl_service_t service;
+    RCCHECK(rclc_service_init_default(&service, 
+			&node, 
+			ROSIDL_GET_SRV_TYPE_SUPPORT(jeeves_msgs, srv, LedSet), 
+			"~/led_set"));
 	// Create timers.
 	// heartbeat timer 
 	rcl_timer_t timer = rcl_get_zero_initialized_timer();
@@ -276,34 +223,27 @@ void micro_ros_task(void * arg)
 		RCL_MS_TO_NS(timer_timeout),
 		hb_timer_callback));
 
-	// timer to handle LED state
-	rcl_timer_t led_timer = rcl_get_zero_initialized_timer();
-	const unsigned int led_timer_timeout = 200; // update LED state every 100ms
-	RCCHECK(rclc_timer_init_default(
-		&led_timer,
-		&support,
-		RCL_MS_TO_NS(led_timer_timeout),
-		led_timer_callback));
+	// Create service callback    
+	jeeves_msgs__srv__LedSet_Response res;
+    jeeves_msgs__srv__LedSet_Request req;
 	// Create executor.
 	rclc_executor_t executor = rclc_executor_get_zero_initialized_executor();
-	RCCHECK(rclc_executor_init(&executor, &support.context, 3, &allocator));
+	RCCHECK(rclc_executor_init(&executor, &support.context, 2, &allocator));
 	unsigned int rcl_wait_timeout = 5000;   // in ms
 	RCCHECK(rclc_executor_set_timeout(&executor, RCL_MS_TO_NS(rcl_wait_timeout)));
 
-	// Add timer and subscriber to executor.
+	// Add timer and Service to executor.
 	RCCHECK(rclc_executor_add_timer(&executor, &timer));
-	RCCHECK(rclc_executor_add_timer(&executor, &led_timer));
-	RCCHECK(rclc_executor_add_subscription(&executor, &subscriber, &recv_msg, &subscription_callback, ON_NEW_DATA));
+	RCCHECK(rclc_executor_add_service(&executor, &service, &req, &res, service_callback));
 
 	// Spin forever.
 	send_msg.data = 0;
 	while(1){
 		rclc_executor_spin_some(&executor, RCL_MS_TO_NS(100));
-		usleep(100000);
+		usleep(1000);
 	}
 
 	// Free resources.
-	RCCHECK(rcl_subscription_fini(&subscriber, &node));
 	RCCHECK(rcl_publisher_fini(&publisher, &node));
 	RCCHECK(rcl_node_fini(&node));
 
